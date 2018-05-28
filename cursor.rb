6a5fe7a9611c63a0d5afa17ad8ffd42bb1694348
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-05-28 17:36:16 -0400

    Refactor cursor

--- cursor.rb
+++ cursor.rb
@@ -88,7 +88,7 @@ class Cursor
   end
 
   def update_pos(diff)
-      new_pos=[@cursor_pos[0] + diff[0], @cursor_pos[1] + diff[1]]
-      @cursor_pos = new_pos if board.in_bounds?(new_pos)
+    new_pos=[@cursor_pos[0] + diff[0], @cursor_pos[1] + diff[1]]
+    @cursor_pos = new_pos if board.in_bounds?(new_pos)
   end
 end
