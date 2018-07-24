8d37d3ff5e2880e88b30da20cf2e6564e6960868
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-07-24 15:35:44 -0400

    Refactor pawn

--- piece/pawn.rb
+++ piece/pawn.rb
@@ -6,16 +6,14 @@ class Pawn < Piece
     "\u2659"
   end
 
-  def move_dirs
-
-  end
-
   def moves
     moves = []
 
     forward_steps.each do |step|
       if board.in_bounds?(step) && board[step] == NullPiece.instance
         moves << step
+      else
+        break
       end
     end
 
@@ -41,14 +39,14 @@ class Pawn < Piece
 
   def forward_steps
     if at_start_row?
-      [[ pos[0] + forward_dir, pos[1]], [pos[0] + (forward_dir+forward_dir), pos[1]] ]
+      [[pos[0] + forward_dir, pos[1]], [pos[0] + (2*forward_dir), pos[1]]]
     else
-      [[ pos[0] + forward_dir, pos[1]]]
+      [[pos[0] + forward_dir, pos[1]]]
     end
   end
 
   def side_attacks
-    attacks = [ [pos[0] + forward_dir, pos[1] + 1], [pos[0] + forward_dir, pos[1] - 1] ]
+    [[pos[0] + forward_dir, pos[1] + 1], [pos[0] + forward_dir, pos[1] - 1]]
   end
 
 end
