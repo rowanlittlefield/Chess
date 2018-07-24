3fc38c0e872252eb32bf81b00068996692bbff0a
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-07-24 09:40:48 -0400

    Decompose chess render method into render position methods

--- display.rb
+++ display.rb
@@ -15,29 +15,26 @@ class Display
     puts "\u2618".colorize(color: :green) + "\u2102" + "\u210B" + " " + "\u2107" + "\u222C" + "\u2618".colorize(color: :green)
     (0...board.grid.length).each do |i|
       (0...board.grid[i].length).each do |j|
-        piece = board[[i, j]]
-        p_color = piece.color
-        if [i, j] == cursor.cursor_pos
-          b_color = :light_yellow
-        else
-          b_color = (i.even? ? even_pattern(j) : odd_pattern(j))
-        end
-        print((board[[i, j]].symbol + " ").colorize(color: p_color, background: b_color, bold: true))
+        render_position(i, j)
       end
       print "\n"
     end
     puts #String.modes
   end
 
-  def display_loop
-    while true
-      render
-      cursor.get_input
-    end
+  private
 
+  def render_position(row, col)
+    piece = board[[row, col]]
+    p_color = piece.color
+    if [row, col] == cursor.cursor_pos
+      b_color = :light_yellow
+    else
+      b_color = (row.even? ? even_pattern(col) : odd_pattern(col))
+    end
+    print((board[[row, col]].symbol + " ").colorize(color: p_color, background: b_color, bold: true))
   end
 
-  private
   def even_pattern(col_num)
     col_num.even? ? :light_red : :light_cyan
   end
--- game.rb
+++ game.rb
@@ -13,24 +13,26 @@ class Game
   end
 
   def play
-    until @board.checkmate(@current_player.color)
-      move = @current_player.make_move
-      if move.is_a?(Array) && @current_player.color == @board[move].color
-        piece = @board[move]
-        completed_turn = false
-        until completed_turn
-          end_pos = @current_player.make_move
-          if end_pos.is_a?(Array) && piece.valid_moves.include?(end_pos)
-            @board.move_piece(move,end_pos)
-            completed_turn = true
-            switch_players
-          end
+    play_turn until @board.checkmate(@current_player.color)
+  end
+
+  private
+
+  def play_turn
+    move = @current_player.make_move
+    if move.is_a?(Array) && @current_player.color == @board[move].color
+      piece = @board[move]
+      completed_turn = false
+      until completed_turn
+        end_pos = @current_player.make_move
+        if end_pos.is_a?(Array) && piece.valid_moves.include?(end_pos)
+          @board.move_piece(move,end_pos)
+          completed_turn = true
+          switch_players
         end
       end
     end
   end
-
-  private
 
   def switch_players
     @current_player = (@current_player == @player1 ? @player2 : @player1)
@@ -38,6 +40,7 @@ class Game
 end
 
 
-
-game = Game.new
-game.play
+if __FILE__ == $PROGRAM_NAME
+  game = Game.new
+  game.play
+end
